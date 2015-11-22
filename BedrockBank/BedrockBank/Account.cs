bcce06334e0fc2e6cbbfef34ea8f918d0237d55d
robertitomaster <morales.robert@live.com>
2015-11-21 23:41:03 -0800

    Bedrock Project

---
+++ BedrockBank/BedrockBank/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    class Account
+    {
+        #region Properties
+
+        public string Name { get; set; }
+
+        public int AccountNumber { get; set; }
+
+        public decimal Balance { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public decimal Deposit(decimal amount)
+        {
+            return 1.0M;
+        }
+        #endregion
+    }
+}
